a1b6e290437e17d8acae21ee02eaaf8a2ef6b3d1
v3lk777 <v3lk777@gmail.com>
2026-07-30 15:41:52 +0300

    Fix ErrorHandler

--- include/errors/error_handler.hpp
+++ include/errors/error_handler.hpp
@@ -48,17 +48,7 @@ public:
             std::cerr << "  " << lineNumber << " | " << lineContent << "\n";
 
             std::string prefix = std::string(std::to_string(lineNumber).size() + 3, ' ');
-            std::cerr << "  | ";
-
-            for (int i = 1; i < token.column; i++) {
-                std::cerr << ' ';
-            }
-
-            for (size_t i = 0; i < token.value.length(); i++) {
-                std::cerr << '^';
-            }
-
-            std::cerr << '\n';
+            std::cerr << prefix << "^" << "\n";
         }
 
         exit(EXIT_FAILURE);
